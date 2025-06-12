b316a4efa87bd8c9d28744f0a3a9ed0bcd4a1922
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-06-12 20:57:17 +0330

    random number

--- 1/index.cpp
+++ 1/index.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-    char x;
-    x = cin.get();
-    cout << x;
+    for (int j = 1; j <= 10; j++)
+    {
+        cout << rand() << endl;
+    }
     return 0;
 }

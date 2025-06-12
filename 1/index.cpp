e3f9c53772e223ac988bc627ba0e66d0a24ae9c9
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-06-12 20:10:51 +0330

    cin.get()

--- 1/index.cpp
+++ 1/index.cpp
@@ -4,22 +4,8 @@ using namespace std;
 
 int main()
 {
-    unsigned int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    switch (n)
-    {
-    case 0:
-        cout << "It is 0";
-        break;
-    case 1:
-        cout << "It is 1";
-        break;
-    case 2:
-        cout << "It is 2";
-        break;
-    default:
-        cout << "default" << endl;
-    }
+    char x;
+    x = cin.get();
+    cout << x;
     return 0;
 }

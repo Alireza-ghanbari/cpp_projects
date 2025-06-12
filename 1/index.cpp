79f7414a607d022c2f7cb433612cea70301f1120
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-06-12 11:36:09 +0330

    fact

--- 1/index.cpp
+++ 1/index.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-    cout << "Enter three real number: " << endl;
-    cin >> a >> b >> c;
-    if ((a < c + b) && (b < a + c) && (c < a + b))
-        cout << "It is a triangle";
-    else
-        cout << "Not a triangle";
+    int n, i;
+    long fact = 1;
+    cout << "Enter a positive integer number: ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+        fact *= i;
+    cout << fact << endl;
     return 0;
 }

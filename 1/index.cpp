fd0643c8e8846adf713a0ca7335b13cbaefe0767
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-06-12 11:53:53 +0330

    print 1 to 10

--- 1/index.cpp
+++ 1/index.cpp
@@ -4,12 +4,8 @@ using namespace std;
 
 int main()
 {
-    int n, i;
-    long s = 0;
-    cout << "Enter a positive integer number: ";
-    cin >> n;
-    for (i = 1; i <= n; i++)
-        s += i;
-    cout << s;
+    int i;
+    for (i = 1; i <= 10; i++)
+        cout << i << endl;
     return 0;
 }

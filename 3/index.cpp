3f7be75d3d1791700811e2da13fe47e8030fdce7
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-06-14 23:49:59 +0330

    print array function

--- 3/index.cpp
+++ 3/index.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
-void modify(int[]);
+void printarr(int[][3]);
 
 int main()
 {
-    int a[5];
-    for (int i = 0; i < 5; i++)
-        a[i] = i + 1;
-    modify(a);
-    for (int i = 0; i < 5; i++)
-        cout << a[i] << endl;
+    int arr1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int arr2[2][3] = {1, 2, 3, 4, 5};
+    int arr3[2][3] = {{1, 2}, {4}};
+    printarr(arr1);
+    cout << endl;
+    printarr(arr2);
+    cout << endl;
+    printarr(arr3);
     return 0;
 }
 
-void modify(int a[])
+void printarr(int a[][3])
 {
-    for (int i = 0; i < 5; i++)
-        a[i] += 2;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << a[i][j] << ' ';
+        }
+        cout << endl;
+    }
 }

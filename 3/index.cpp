ebea5d840bba69e0fd7f569c5df238b7d7913e23
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-06-15 00:18:46 +0330

    call by reference

--- 3/index.cpp
+++ 3/index.cpp
@@ -2,29 +2,26 @@
 
 using namespace std;
 
-void printarr(int[][3]);
+int vfunct(int);
+void rfunct(int &);
 
 int main()
 {
-    int arr1[2][3] = {{1, 2, 3}, {4, 5, 6}};
-    int arr2[2][3] = {1, 2, 3, 4, 5};
-    int arr3[2][3] = {{1, 2}, {4}};
-    printarr(arr1);
-    cout << endl;
-    printarr(arr2);
-    cout << endl;
-    printarr(arr3);
+    int x = 5, y = 10;
+    cout << x << endl
+         << vfunct(x) << endl
+         << x << endl;
+    cout << y << endl;
+    rfunct(y);
+    cout << y << endl;
     return 0;
 }
 
-void printarr(int a[][3])
+int vfunct(int a)
 {
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << a[i][j] << ' ';
-        }
-        cout << endl;
-    }
+    return a *= a;
+}
+void rfunct(int &b)
+{
+    b *= b;
 }

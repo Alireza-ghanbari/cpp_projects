cde8705968e543bc65ece4f8b21fed59abd10300
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-08-14 22:06:10 +0430

    string replace

--- index.cpp
+++ index.cpp
@@ -6,32 +6,19 @@ using namespace std;
 
 int main()
 {
-    string text;
+    char str [201];
     cout << "Enter text: ";
-    getline(cin, text);
+    cin.getline(str, 201);
 
-    int wordCount = 0, sentenceCount = 0;
-    bool inWord = false;
+    string s = str;
 
-    for(char ch : text){
-        if(isspace(ch)){
-            if(inWord){
-                wordCount++;
-                inWord = false;
-            }
-        }else if(isalpha(ch)){
-            inWord = true;
-        }
-
-        if(ch == '.' || ch == '?' || ch == '!'){
-            sentenceCount++;
-        }
+    size_t pos = 0;
+    while((pos = s.find("IS", pos)) != string::npos){
+        s.replace(pos, 2, "WHERE"),
+        pos += 5;
     }
 
-    if(inWord) wordCount++;
-
-    cout << "Word count: " << wordCount << endl;
-    cout << "sentence count: " << sentenceCount << endl;
+    cout << "Resault: " << s << endl;
 
     return 0;
 }

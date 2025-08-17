66cdd4e2163d885389b95e358f2ea7ba0eea185c
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-08-17 13:01:31 +0430

    Banking Program

---
+++ Banking Program/index.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+
+int main()
+{
+    double balance = 0;
+    int choice = 0;
+
+    do
+    {
+        cout << "*******************" << endl;
+        cout << "Enter yout choice: " << endl;
+        cout << "*******************" << endl;
+        cout << "1. Show blance" << endl;
+        cout << "2. Deposit Money" << endl;
+        cout << "3. Withdraw Money" << endl;
+        cout << "4. Exit" << endl;
+
+        cin >> choice;
+
+        cin.clear();
+        fflush(stdin);
+
+        switch (choice)
+        {
+        case 1:
+            showBalance(balance);
+            break;
+        case 2:
+            balance += deposit();
+            showBalance(balance);
+            break;
+        case 3:
+            balance -= withdraw(balance);
+            showBalance(balance);
+            break;
+        case 4:
+            cout << "Thanks for visiting" << endl;
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+            break;
+        }
+    } while (choice != 4);
+
+    return 0;
+}
+
+void showBalance(double balance)
+{
+    cout << "Your balance is: $" << setprecision(2) << fixed << balance << endl;
+}
+
+double deposit()
+{
+    double amount = 0;
+    cout << "Enter amount to be deposited: ";
+    cin >> amount;
+
+    if (amount > 0)
+    {
+        return amount;
+    }
+    else
+    {
+        cout << "That's not a valid amount: " << endl;
+        return 0;
+    }
+}
+
+double withdraw(double balance)
+{
+    double amount = 0;
+    cout << "Enter amount to be withdrawn: ";
+    cin >> amount;
+
+    if (amount > balance)
+    {
+        cout << "Insufficient funds" << endl;
+        return 0;
+    }
+    else if (amount < 0)
+    {
+        cout << "That's not a valid amount" << endl;
+        return 0;
+    }
+    else
+    {
+        return amount;
+    }
+}
